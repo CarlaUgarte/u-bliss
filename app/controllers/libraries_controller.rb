142e197f890a17d50b9959b8aa22ca29f3f61232
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2024-12-10 13:10:22 -0600

    Avance controller libraries

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,17 +1,32 @@
 class LibrariesController < ApplicationController
-  def index
 
+  def index
+    @libraries = Library.all
   end
 
   def show
-
+    @library = Library.find(params[:id])
   end
 
   def create
+    @library = Library.new(library_params)
+    if @library.save
+      redirect_to @library, notice: "La biblioteca fue creada con éxito.."
+    else
+      render :new status: :unprocessable_entity
 
   end
 
   def destroy
-    
+    @library = Library.find(params[:id])
+    @library.destroy
+    redirect_to libraries_url, notice: "La biblioteca fue destruida con éxito."
   end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:user_id, :syllabus_id)
+  end
+
 end
