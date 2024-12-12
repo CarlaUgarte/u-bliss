b51e28094b2dcc74f9ea0b2dc8d35bbdfca8e6ce
Sandra Alvarez <sandra.alvarez.ramos18@gmail.com>
2024-12-12 10:17:06 -0600

    gema cloudinary

--- app/models/syllabus.rb
+++ app/models/syllabus.rb
@@ -3,6 +3,7 @@ class Syllabus < ApplicationRecord
   belongs_to :category
   has_many :syllabus_modules, dependent: :destroy
   has_many :libraries
+  has_one_attached :image
 
   validates :title, presence: true
   validates :description, presence: true
