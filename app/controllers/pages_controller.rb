55629d8ced9bde9ea42cc7c604caab9127286cea
sebastian flores <saflchewis@live.com>
2024-12-18 08:48:03 -0500

    asd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,19 @@ class PagesController < ApplicationController
     @user = current_user
   end
 
+  def update_profile
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to my_profile_path, notice: 'Perfil actualizado exitosamente.'
+    else
+      render :my_profile
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :description, :linkedin, :avatar)
+  end
+
 end
